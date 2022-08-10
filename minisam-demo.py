c1a23d8b2d687c4a2ff27e8ff18e10ef101ce181
Andy Piper <andypiper@users.noreply.github.com>
2022-08-10 17:46:41 +0100

    Update minisam-demo.py
    quick debounce code

--- minisam-demo.py
+++ minisam-demo.py
@@ -5,25 +5,28 @@
 # gh repo clone mattytrentini/micropython-dotstar
 from micropython_dotstar import DotStar
 from machine import Pin, SoftSPI, LED
+import time
 
 # setup the RGB LED on the left leg
 # final param (miso) can be any unused pin
 spi = SoftSPI(sck=Pin("DOTSTAR_CLK"), mosi=Pin("DOTSTAR_DATA"), miso=Pin(17)) 
 dot = DotStar(spi, 1) # single LED (DotStar supports multiple, board has one)
 
-# todo - some kind of rainbow cycle
-dot[0] = (25, 128, 20, 0.1) # light the LED blue
+# todo - some kind of rainbow cycle to run in background
+dot[0] = (5, 12, 59, 0.3) # light the LED 
 
-# turn on the LED
+# turn on the red chest LED
 led = LED("LED")
-
-# todo - debounce button press
 button = Pin("BUTTON", Pin.IN, Pin.PULL_UP)
 
+# trivial button press debounce could be improved
 while True:
-    if button.value():
-        print("Button pressed")
-        led.on()
-    else:
-        led.off()
+    first = button.value()
+    time.sleep(0.01)
+    second = button.value()
+    if first and not second:
+        led.toggle()
+        print('Button pressed, toggle LED')
+
+
 
